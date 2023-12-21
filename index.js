76535e255a671d5ed6139fc29040b6e9b171d8ba
juanda <juandacorreo@gmail.com>
2023-12-21 09:08:31 +0100

    Primer commit

---
+++ index.js
@@ -0,0 +1,5 @@
+let numero1 = '30'
+numero1 = 6
+console.log(numero1)
+
+// adsñkljadsf j ñasjdfñajds kfñaslkj añlkjf añlskdfjñlkdfj añlsdjfk aslñkdjf asñlkfj asñdlfasdfkjl asdfklñj adfñlkj asñdlfjkasdfjñlk asdfñjkl dfsj
